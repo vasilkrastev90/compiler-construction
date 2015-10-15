18b6111c212ae5057f795b0b6fda72598eeb77de
Vasil Krastev <vasilkrastev90@yahoo.com>
2015-10-15 20:11:33 +0100

    test suite finished

--- lan_test.ml
+++ lan_test.ml
@@ -10,11 +10,11 @@ let print_position lexbuf =   let pos = lexbuf.lex_curr_p in
 
 let parse_with_error lexbuf =   try Lan_par.top Lan_lex.read lexbuf with   
 | SyntaxError msg -> prerr_string (msg ^ ": ");                        
-  print_position lexbuf;                        
-  exit (-1)   
+  print_position lexbuf;
+  []                           
 | Lan_par.Error ->   prerr_string "Parse error:";                        
   print_position lexbuf;                        
-  exit (-1)
+  []
 
 let rec loop_interactive buf =
   let line = read_line () in
@@ -53,9 +53,22 @@ let execute_tests () =
          |>printf("id = 4+5-3 should be parsed correctly  Assign (Id \"id\", Minus(Plus(Int 4, Int 5),Int 3)): %B\n");
     parse_with_error(Lexing.from_string "4;5+3;id") = [Int 4; Plus(Int 5, Int 3);IdExp (Id "id")]
          |>printf("4;5+3;id should be parsed correctly [Int 4, Plus(Int 5, Int 3),IdExp (Id \"id\")]: %B\n");
+    parse_with_error (Lexing.from_string "x =print z + 13 - 25*6 + read q + print 5") = [Assign(Id "x",Minus(Plus(Write (IdExp (Id "z")),Int 13),Plus(Plus(Times(Int 25,Int 6),Read (Id "q")),Write (Int 5))))]
+         |> printf ("x =print z + 13 - 25*6 + read q + print 5 should be parsed Assign(Id \"x\",Minus(Plus(Write (IdExp (Id \"z\")),Int 13),Plus(Plus(Times(Int 25,Int 6),Read (Id \"q\")),Write (Int 5)))): %B\n");
     parse_with_error (Lexing.from_string " ") = []
-         |> printf("empty string should be parsed correctly as empty list: %B\n")     
-
+         |> printf("empty string should be parsed correctly as empty list: %B\n");
+    parse_with_error (Lexing.from_string "4;") = []
+         |> printf("last expresion in the list:4; terminating with semicolon should cause a parsing error: %B\n");
+    parse_with_error (Lexing.from_string "3;5+2;var;") = []
+         |> printf ("last expresion in the list:3;5+2;var; terminating with semicolon should cause a parsing error: %B\n");
+    parse_with_error(Lexing.from_string "x==3") = [] 
+         |> printf("incorrect token in assignment: x==3 should cause parse error %B\n");
+    parse_with_error(Lexing.from_string "3=3") = []
+         |> printf("incorrect token in assignment: 3=3 should cause parse error %B\n");
+    parse_with_error(Lexing.from_string "read 3") = []
+         |> printf("trying to read an integer in read 3 should cause parse error: %B\n");
+    parse_with_error (Lexing.from_string "3++5") = []
+         |> printf ("duplicated + symbols should cause an error: %B\n")
 
 let _ = if Array.length Sys.argv >1  && Sys.argv.(1) = "test" then 
            execute_tests ()
